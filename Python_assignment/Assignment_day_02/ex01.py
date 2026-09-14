00f643a6cefe18f257b32a6d687ff133cdb37145
Saumya Bajpai <134991597+Bajpaisaumya@users.noreply.github.com>
2026-09-15 00:41:14 +0530

    Create ex01.py

---
+++ Python_assignment/Assignment_day_02/ex01.py
@@ -0,0 +1,8 @@
+def main():
+    inp = input("Sample Input: ")
+    print("Sample Output:")
+    print(f"Total Characters: {len(inp)}")
+    print(f"Total Words: {len(inp.split())}")
+
+if __name__ == "__main__":
+    main()
